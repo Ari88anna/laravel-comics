6745c744e8f89b7e8af76b14cae12ca23a9ea1e3
Arianna Carotti <arianna.carotti@gmail.com>
2021-06-08 16:28:22 +0200

    cominciato jumbotron

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&family=Open+Sans:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
+    <link rel="shortcut icon" href="{{asset('img/favicon.ico')}}">
+
 </head>
 <body>
     
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -17,5 +17,82 @@
     </div>
     {{-- end Upper header --}}
 
+    {{-- Nav --}}
+    <nav>
+
+        <div class="container">
+
+            <div class="header-nav">
+                {{-- Logo --}}
+                <div class="logo">
+                    <img src="{{ asset('img/dc-logo.png') }}" alt="Digital Comics Logo">
+                </div>
+                {{-- end logo --}}
+
+                {{-- Menu --}}
+                <ul class="menu">
+
+                    <li>
+                        <a href="#">Characters</a>
+                    </li>
+
+                    <li class="active">
+                        <a href="#">Comics</a>
+                    </li>
+
+                    <li>
+                        <a href="#">Movies</a>
+                    </li>
+
+                    <li>
+                        <a href="#">Tv</a>
+                    </li>
+
+                    <li>
+                        <a href="#">Games</a>
+                    </li>
+
+                    <li>
+                        <a href="#">Collectibles</a>
+                    </li>
+
+                    <li>
+                        <a href="#">Videos</a>
+                    </li>
+
+                    <li>
+                        <a href="#">Fans</a>
+                    </li>
+
+                    <li>
+                        <a href="#">News</a>
+                    </li>
+
+                    <li>
+                        <a href="#">Shop <i class="fas fa-caret-down"></i></a>
+                    </li>                  
+
+                </ul>
+                {{-- end Menu --}}
+
+                {{-- Searchbar --}}
+                <div class="searchbar">
+                    <input type="text" placeholder="Search">
+                </div>
+                {{-- end Searchbar --}}
+
+
+            </div>
+
+        </div>
+
+    </nav>
+    {{-- Nav --}}
+
+    {{-- Jumbotron --}}
+    <div class="jumbotron">
+
+    </div>
+    {{-- end Jumbotron --}}
     
 </header>
