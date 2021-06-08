9c38930f98e700f60d65275b13b6ede42026c6f0
Arianna Carotti <arianna.carotti@gmail.com>
2021-06-08 15:20:32 +0200

    aggiunto option a webpack.mix e cominciato header

--- resources/views/partials/footer.blade.php
+++
@@ -1,1 +0,0 @@
-<h2>sono footer</h2>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,21 @@
-<h1>sono header</h1>
+<header>
+    {{-- Upper header --}}
+    <div class="upper-header">
+        <div class="container">
+
+            <ul>
+                <li>  
+                    <a href="#">DC POWER VISA</a>
+                </li>
+                
+                <li>
+                    <a href="#">ADDITIONAL DC SITES <i class="fas fa-caret-down"></i></a>
+                </li>
+            </ul>
+
+        </div>
+    </div>
+    {{-- end Upper header --}}
+
+    
+</header>
